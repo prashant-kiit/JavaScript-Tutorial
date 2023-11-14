e465b6b0f4cb1d7e986177bbcf4fcf6c8d0bf28c
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2023-11-15 00:29:24 +0530

    3rd push

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ let person = {
 console.log(person);
 console.log(person.sender);
 console.log(person['message']);
+
+
+let list = ["Ravana", 67];
+list[5] = "Kansa";
+console.log(list);
+console.log(list[0]);
+console.log(list[1]);
+
+function greet(name){
+    console.log("Hello " + name);
+}
+
+greet("Balarama");
+greet("Bheem");
+greet(typeof greet);
+
+console.log(nickName.constructor);
