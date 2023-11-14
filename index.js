7946a1ef70270692b458a4a650ce35e79863d53b
Prashant Kumar Singh <prashantsingh090798@gmail.com>
2023-11-14 16:05:05 +0530

    1st push

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Hello World! How are you?');
+let myName = "Prashant";
+const nickName = "Chinku";
+let surname = "Singh";
+console.log(myName);
+myName = 56;
+console.log(myName);
+console.log(nickName);
+console.log(surname);
