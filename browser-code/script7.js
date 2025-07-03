dae29c2810cfb71a6b438b141d9a1b9c1b38a8ae
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-03 18:21:57 +0530

    feat: block scope, function scope and arrow function

---
+++ browser-code/script7.js
@@ -0,0 +1,15 @@
+console.log("Main Running");
+
+function parent() {
+    console.log("Parent Running");
+    for (let i = 0; i < 5; i++) {
+        console.log("i", i);
+    }
+    for (var k = 0; k < 5; k++) {
+        console.log("k", k);
+    }
+    
+    console.log("Parent Running");
+}
+
+parent();
