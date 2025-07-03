873ab2717425292a28e69c643af15c1805adef84
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-03 18:17:35 +0530

    feat: var and function

---
+++ browser-code/script1.js
@@ -0,0 +1,17 @@
+console.log("Main Running");
+
+var a = 10;
+function parent() {
+    var b = 20;
+    function child() {
+        var c = 30;
+        function grandChild() {
+            console.log("a", a);
+            console.log("b", b);
+            console.log("c", c);
+        }
+        grandChild();
+    }
+    child();
+}
+parent();
