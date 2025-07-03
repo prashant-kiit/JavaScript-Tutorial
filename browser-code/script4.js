f3ee3cfd19ff4f37c06c66e0c7650a96f3b49439
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-03 18:20:00 +0530

    feat: scope and arrow function

---
+++ browser-code/script4.js
@@ -0,0 +1,18 @@
+console.log("Main Running");
+
+var a = 10;
+const parent = () => {
+    var b = 20;
+    const child = () => {
+        var c = 30;
+        const grandChild = () => {
+            console.log("a", a);
+            console.log("b", b);
+            console.log("c", c);
+            console.log("this", this);
+        }
+        grandChild();
+    }
+    child();
+}
+parent();
