99e180ea2cc732fa5aa334bcd73d7b85c1ca44b9
prashant-m4 <prashantsingh090798@gmail.com>
2025-07-04 15:11:02 +0530

    refactor: change count tag id to count

--- Custom-React/App.js
+++ Custom-React/App.js
@@ -1,14 +1,14 @@
 import { registerForHydration, STATE } from "./React.js";
 
 function App() {
-    const count = new STATE("hello", 0);
+    const count = new STATE("count", 0);
     count.setState("btn-inc", "click", (value) => ++value);
     count.setState("btn-dec", "click", (value) => --value);
     // TODO: useEffect
     
     return ` 
         <h2>Welcome to Counter</h2>
-        <p> Count is <span id="hello">${count.getState()}</span></span></p>
+        <p> Count is <span id="count">${count.getState()}</span></span></p>
         <button id="btn-inc">Increment</button>
         <button id="btn-dec">Decrement</button>
     `
